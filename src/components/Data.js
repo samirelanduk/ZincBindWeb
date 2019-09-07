c957c25c2cc4e6c38131f935d15b5c0c7b1d31d2
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-07 19:39:14 +0100

    Data page outline

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Box from "./Box";
 
 class Data extends Component {
     constructor(props) {
@@ -6,7 +8,45 @@ class Data extends Component {
         this.state = {  }
     }
     render() { 
-        return ( <div>Data</div> );
+        return (
+            <main className="data">
+                <Box>
+                    <h1>ZincBind Data</h1>
+                </Box>
+                    
+                <Box>
+                    <p>The full list of Zinc Binding Sites can be browsed <Link to="/data/all/">here</Link>.
+                    They can also be browsed by Family and Group <Link to="/families/">here</Link>.
+                    See the <a href="https://api.zincbind.net">GraphQL API</a> for programmatic access to the data.</p>
+                </Box>
+
+                <div className="charts">
+                    <Box>
+                        chart1
+                    </Box>
+
+                    <Box>
+                        chart2
+                    </Box>
+
+                    <Box>
+                        chart3
+                    </Box>
+
+                    <Box>
+                        chart4
+                    </Box>
+
+                    <Box>
+                        chart5
+                    </Box>
+
+                    <Box>
+                        chart6
+                    </Box>
+                </div>
+            </main>
+        );
     }
 }
  
