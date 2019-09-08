5011d1ceaba7a57c9881301478f71f1d43cfcfa6
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-08 20:19:22 +0100

    Make home values pull from server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import { Link } from "react-router-dom";
 import zincsite from '../images/zincsite.png';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
+    
+    render() {
+        const QUERY = gql`{ stats { pdbCount allSiteCount uniqueSiteCount } }`;
+
         return (
             <main className="home">
                 <div className="home-row home-1">
                     <div className="home-cell site-description">
                         <h1>The Database of Zinc Binding Sites</h1>
-                        <p>ZincBind is a database of zinc binding sites, automatically generated from the Protein Data Bank. There are currently 6,458 unique sites, with 31,753 representatives across 14,657 PDB files.</p>
+                        <Query query={QUERY} >
+                            {
+                                ({data, loading}) => {
+                                    if (loading) {
+                                        return <p></p>
+                                    }
+
+                                    const {pdbCount, allSiteCount, uniqueSiteCount} = data.stats;
+                                    return <p>ZincBind is a database of zinc binding sites, automatically generated from the Protein Data Bank. There are currently {uniqueSiteCount.toLocaleString()} unique sites, with {allSiteCount.toLocaleString()} representatives across {pdbCount.toLocaleString()} PDB files.</p>
+                                }
+                            }
+                        </Query>
+                        
                     </div>
                     <div className="home-cell site-search">
                         <div className="small-links"><Link to="/data/all/">Browse all sites</Link></div>
                         <form action="/search">
-                            <input autocomplete="off" name="q" placeholder="PDB code, description, etc." type="text" />
+                            <input autoComplete="off" name="q" placeholder="PDB code, description, etc." type="text" />
                             <input type="submit" value="Search" />
                         </form>
                     </div>
