1fd8b434ccab969e3ec9734356209493d6e5e469
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-08 17:54:29 +0100

    Fix target='_blank' problem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,19 +9,19 @@ class Footer extends Component {
                 <div className="links">
                     <div className="useful">
                         <h3>Useful Links</h3>
-                        <a href="http://www.bioinf.org.uk/" target="_blank">The ACRM Group</a>
-                        <a href="https://github.com/samirelanduk/zincbind" target="_blank">Source code</a>
+                        <a href="http://www.bioinf.org.uk/" target="_blank" rel="noopener noreferrer">The ACRM Group</a>
+                        <a href="https://github.com/samirelanduk/zincbind" target="_blank" rel="noopener noreferrer">Source code</a>
                         <Link to="/changelog/">Changelog</Link>
                     </div>
                     <div className="related">
                         <h3>Related Projects</h3>
-                        <a href="http://metalweb.cerm.unifi.it/" target="_blank">MetalPDB</a>
-                        <a href="http://synpharm.guidetopharmacology.org/" target="_blank">SynPHARM</a>
-                        <a href="https://www.bindingdb.org/bind/index.jsp" target="_blank">The Binding Database</a>
+                        <a href="http://metalweb.cerm.unifi.it/" target="_blank" rel="noopener noreferrer">MetalPDB</a>
+                        <a href="http://synpharm.guidetopharmacology.org/" target="_blank" rel="noopener noreferrer">SynPHARM</a>
+                        <a href="https://www.bindingdb.org/bind/index.jsp" target="_blank" rel="noopener noreferrer">The Binding Database</a>
                     </div>
                 </div>
                 <div className="attribution">
-                    Created by: <a target="_blank" href="https://samireland.com/">Sam Ireland</a>
+                    Created by: <a target="_blank" rel="noopener noreferrer" href="https://samireland.com/">Sam Ireland</a>
                 </div>
             </footer>
         );
