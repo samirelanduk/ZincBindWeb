e3ab4347d000a2d59d527539bdd0eb93bdff2110
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-14 14:25:33 +0100

    Add pagination

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Box from "./Box";
@@ -9,20 +9,25 @@ class AllData extends Component {
     
     render() {
 
-        const QUERY = gql`query pdbs($sort: String) { pdbs(sort: $sort, first: 25) { edges { node {
+        const QUERY = gql`query pdbs($sort: String, $skip: Int) { pdbs(sort: $sort, first: 25, skip: $skip) { edges { node {
             id depositionDate organism title classification technique resolution zincsites {
                 edges { node { id residues { edges { node { id atomiumId }}} } }
             }
-        } } } }`;
+        } } } count: stats { pdbCount }}`;
 
         let params = this.props.history.location.search;
         let keywords = params.slice(1).split("&");
         let sort = "-deposition_date";
+        let skip = 0;
         for (let keyword of keywords) {
             if (keyword.split("=")[0] === "sort") {
                 sort = keyword.split("=")[1];
             }
+            if (keyword.split("=")[0] === "page") {
+                skip = (parseInt(keyword.split("=")[1]) - 1) * 25;
+            }
         }
+        
 
         return (
             <main className="all-data search-results">
@@ -30,19 +35,25 @@ class AllData extends Component {
                     <h1>All Data</h1>
                 </Box>
 
-                <SearchNav history={this.props.history} sort={sort} />
+                
 
-                <Query query={QUERY} variables={{sort: sort}} >
+                <Query query={QUERY} variables={{sort: sort, skip: skip}} >
                     {
                         ({loading, data}) => {
                             if (loading) {
                                 return <div className="results"></div>
                             }
-                            return <div className="results">{
-                                data.pdbs.edges.map((edge) => {
-                                    return <SearchResult pdb={edge.node} key={edge.node.id} />
-                                })
-                            }</div>
+                            return (
+                                <Fragment>
+                                <SearchNav history={this.props.history} sort={sort} count={data.count.pdbCount} />
+                                <div className="results">{
+                                    data.pdbs.edges.map((edge) => {
+                                        return <SearchResult pdb={edge.node} key={edge.node.id} />
+                                    })
+                                }</div>
+                                <SearchNav history={this.props.history} sort={sort} count={data.count.pdbCount} />
+                                </Fragment>
+                            );
                             
                         }
                     }
--- src/components/SearchNav.js
+++ src/components/SearchNav.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Box from "./Box";
 
 class SearchNav extends Component {
@@ -7,22 +8,63 @@ class SearchNav extends Component {
         this.props.history.push(`${this.props.history.location.pathname}?sort=${event.target.value}`);
     }
 
+    makeParamsString = (params, page) => {
+        let string = "";
+        for (let key of Object.keys(params)) {
+            string += key + "=" + (key === "page" ? page : params[key]) + "&";
+        }
+        return string.slice(0, string.length - 1);
+    }
+
     render() {
+
+        let path = this.props.history.location.pathname;
+
+        let params = {};
+
+        for (let pair of this.props.history.location.search.slice(1).split("&")) {
+            pair = pair.split("=");
+            if (pair[0]) {
+                params[pair[0]] = pair[1];
+            }
+           
+        }
+        params.page = params.page || 1;
+
+        const pageCount = Math.ceil(this.props.count / 25);
+
+        let oneLink = this.makeParamsString(params, 1);
+        let nextLink = this.makeParamsString(params, parseInt(params.page) + 1);
+        let prevLink = this.makeParamsString(params, parseInt(params.page) - 1);
+
+        let lastLink = this.makeParamsString(params, pageCount);
+
+
         return (
-            <div className="seartch-nav">
+            <div className="search-nav">
                 <Box>
-                <select onChange={this.sort} value={this.props.sort}>
-                    <option value="-deposition_date">Newest to Oldest</option>
-                    <option value="deposition_date">Oldest to Newest</option>
-                    <option value="resolution">Resolution (Best to Worst)</option>
-                    <option value="-resolution">Resolution (Worst to Best)</option>
-                    <option value="rvalue">R-value (Best to Worst)</option>
-                    <option value="-rvalue">R-value (Worst to Best)</option>
-                    <option value="title">PDB Title (A-Z)</option>
-                    <option value="-title">PDB Title (Z-A)</option>
-                    <option value="id">PDB Code (A-Z)</option>
-                    <option value="-id">PDB Code (Z-A)</option>
-                </select>
+                    <select onChange={this.sort} value={this.props.sort}>
+                        <option value="-deposition_date">Newest to Oldest</option>
+                        <option value="deposition_date">Oldest to Newest</option>
+                        <option value="resolution">Resolution (Best to Worst)</option>
+                        <option value="-resolution">Resolution (Worst to Best)</option>
+                        <option value="rvalue">R-value (Best to Worst)</option>
+                        <option value="-rvalue">R-value (Worst to Best)</option>
+                        <option value="title">PDB Title (A-Z)</option>
+                        <option value="-title">PDB Title (Z-A)</option>
+                        <option value="id">PDB Code (A-Z)</option>
+                        <option value="-id">PDB Code (Z-A)</option>
+                    </select>
+
+                    <div className="search-links">
+                        {params.page > 2 && <Link className="search-link" to={`${path}?${oneLink}`}>1</Link>}
+                        {params.page > 3 && <div className="search-link">...</div>}
+                        {params.page > 1 && <Link className="search-link" to={`${path}?${prevLink}`}>{parseInt(params.page) - 1}</Link>}
+                        <div className="search-link">{params.page}</div>
+                        {params.page < pageCount - 1 && <Link className="search-link" to={`${path}?${nextLink}`}>{parseInt(params.page) + 1}</Link>}
+                        {params.page < pageCount - 2 && <div className="search-link">...</div>}
+                        {params.page < pageCount && <Link className="search-link" to={`${path}?${lastLink}`}>{pageCount}</Link>}
+                    </div>
                 </Box>
             </div>
         );
