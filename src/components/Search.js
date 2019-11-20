2aff7c64303a06da6bd63f796ce568efb4cf7c5f
Sam Ireland <sam.ireland.uk@gmail.com>
2019-11-20 18:37:03 +0000

    Refactor PDB search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,52 +7,64 @@ class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            terms: [{deletable: false, selected: "title", value: ""}],
+            pdbSelect: [
+                ["title", "Title contains", "e.g. antibody, carbonic anhydrase"],
+                ["classification", "Classification contains", "e.g. oxidoreductase, membrane"],
+                ["keywords", "Keywords contain", "e.g. ion channel, zinc finger"],
+                ["organism", "Organism contains", "e.g. homo sapiens"],
+                ["expression", "Expression System contains", "e.g. saccharomyces"],
+                ["technique", "Technique contains", "e.g. NMR"],
+                ["resolution_lt", "Resolution better than", "e.g. 2.5"],
+                ["resolution_gt", "Resolution worse than", "e.g. 10"],
+                ["rfactor_lt", "Rfactor better than", "e.g. 0.2"],
+                ["rfactor_gt", "Rfactor worse than", "e.g. 0.3"],
+                ["deposited_lt", "Deposited since", "e.g. 2017-01-01"],
+                ["deposted_gt", "Deposited before", "e.g. 2000-01-01"]
+            ],
+            pdbTerms: [[0, ""]],
+            siteTerms: [],
             blastString: "",
             expectString: "10"
         }
+        let siteSelect = [];
+        for (const row of this.state.pdbSelect) {
+            siteSelect.push([row[0], row[1], row[2]]);
+        }
+        this.state.siteSelect = siteSelect;
     }
 
     addTerm = () => {
-        let terms = this.state.terms;
-        for (let term of terms) {
-            term.deletable = true;
-        }
-        terms.push({deletable: true, selected: "title", value: ""});
+        let terms = this.state.pdbTerms;
+        terms.push([0, ""]);
+        this.setState({pdbTerms: terms});
+    }
+
+    removeTerm = (e) => {
+        const index = parseInt(e.target.parentNode.getAttribute("data-index"));
+        let terms = this.state.pdbTerms;
+        terms.splice(index, 1);
         this.setState({terms: terms});
     }
 
     updateSelect = (e) => {
         const index = parseInt(e.target.parentNode.getAttribute("data-index"));
-        let terms = this.state.terms;
-        terms[index].selected = e.target.value;
-        this.setState({terms: terms});
+        let terms = this.state.pdbTerms;
+        terms[index] = [e.target.selectedIndex, ""];
+        this.setState({pdbTerms: terms});
     }
 
-    handleInputChange = (e) => {
+    updateInput = (e) => {
         const index = parseInt(e.target.parentNode.getAttribute("data-index"));
-        let terms = this.state.terms;
-        terms[index].value = e.target.value;
-        this.setState({terms: terms});
-    }
-
-    removeTerm = (e) => {
-        const index = parseInt(e.target.parentNode.getAttribute("data-index"));
-        let terms = this.state.terms;
-        terms.splice(index, 1);
-        if (terms.length === 1) {
-            for (let term of terms) {
-                term.deletable = false;
-            }
-        }
-        this.setState({terms: terms})
+        let terms = this.state.pdbTerms;
+        terms[index][1] = e.target.value;
+        this.setState({pdbTerms: terms});
     }
 
     search = () => {
         let query = [];
-        for (let term of this.state.terms) {
-            if (term.value) {
-                query.push(`${term.selected}=${term.value}`);
+        for (let term of this.state.pdbTerms) {
+            if (term[1]) {
+                query.push(`${this.state.pdbSelect[term[0]][0]}=${term[1]}`);
             }
         }
         query = query.join("&");
@@ -84,42 +96,31 @@ class Search extends Component {
         }
         return (
             <main className="search">
-                <Box>
-                    <h1>Advanced Search</h1>
-                </Box>
+                <Box><h1>Advanced Search</h1></Box>
                 
                 <Box>
                     <h2>PDB Search</h2>
-                        {this.state.terms.map((term, i) => {
-                            return <div className="search-input" key={i} data-index={i}>
-                                <select value={term.selected} onChange={this.updateSelect}>
-                                    <option data-placeholder="e.g. antibody, carbonic anhydrase" value="title">Title contains</option>
-                                    <option data-placeholder="e.g. oxidoreductase, membrane" value="classification">Classification contains</option>
-                                    <option data-placeholder="e.g. ion channel, zinc finger" value="keywords">Keywords contain</option>
-                                    <option data-placeholder="e.g. homo sapiens" value="organism">Organism contains</option>
-                                    <option data-placeholder="e.g. saccharomyces" value="expression">Expression System contains</option>
-                                    <option data-placeholder="e.g. NMR" value="technique">Technique contains</option>
-                                    <option data-placeholder="e.g. 2.5" value="resolution_lt">Resolution better than</option>
-                                    <option data-placeholder="e.g. 10" value="resolution_gt">Resolution worse than</option>
-                                    <option data-placeholder="e.g. 0.2" value="rfactor_lt">Rfactor better than</option>
-                                    <option data-placeholder="e.g. 0.3" value="rfactor_gt">Rfactor worse than</option>
-                                    <option data-placeholder="e.g. 2017-01-01" value="deposited_gt">Deposited since</option>
-                                    <option data-placeholder="e.g. 2000-01-01" value="deposited_lt">Deposited before</option>
-                                    {/* <option data-placeholder="e.g. H3, C2H2" value="family">Family is</option>
-                                    <option data-placeholder="e.g. H3, C2" value="code">Family contains</option>
-                                    <option data-placeholder="e.g. GLU, BON" value="residue_names">Residue names contain</option> */}
+                    {this.state.pdbTerms.map((term, i) => {
+                        return (
+                            <div className="search-input" key={i} data-index={i}>
+                                <select value={this.state.pdbSelect[term[0]][0]} onChange={this.updateSelect}>
+                                    {this.state.pdbSelect.map((option, o) => {
+                                        return <option key={o} value={option[0]}>{option[1]}</option>
+                                    })}
                                 </select>
-                                <input type="text" onChange={this.handleInputChange}></input>{term.deletable && <button className="remove-term" onClick={this.removeTerm}>×</button>}
+                                <input type="text" placeholder={this.state.pdbSelect[term[0]][2]} onChange={this.updateInput} value={term[1]}></input>{
+                                    this.state.pdbTerms.length > 1 && <button className="remove-term" onClick={this.removeTerm}>×</button>
+                                }
                             </div>
-                        })}
-                        
-
+                        )
+                    })}
+                    
                     <div className="search-buttons">
                         <button onClick={this.addTerm}>New Term</button>
                         <button onClick={this.search}>Search</button>
-                    </div>
-                    
+                    </div> 
                 </Box>
+
 
                 <Box>
                     <h2>Site Search</h2>
