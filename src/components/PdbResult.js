9e3073123411afc643d7b676f7139c1c41887ad8
Sam Ireland <sam.ireland.uk@gmail.com>
2019-11-24 13:51:43 +0000

    PDB result style

--- src/components/PdbResult.js
+++ src/components/PdbResult.js
@@ -16,21 +16,28 @@ class PdbResult extends Component {
     
     render() { 
         return (
-            <div className="search-result"><Box>
-                <div className="row-1">
-                    <div className="id">{ this.props.pdb.id }</div>
-                    <div className="date">{ this.formatDate(this.props.pdb.depositionDate) }</div>
-                    <div className="species">{ this.props.pdb.organism }</div>
+            <div className="pdb-result"><Box>
+                <div className="pdb">
+                    <div className="row-1">
+                        <div className="id">{ this.props.pdb.id }</div>
+                        <div className="date">{ this.formatDate(this.props.pdb.depositionDate) }</div>
+                        <div className="species">{ this.props.pdb.organism }</div>
+                    </div>
+                    <div className="row-2">
+                        <div className="title">{ this.props.pdb.title }</div>
+                    </div>
+                    <div className="row-3">
+                        <div className="classification">{ this.props.pdb.classification }</div>
+                        <div className="technique">{ this.props.pdb.technique }</div>
+                        <div className="resolution">{ (this.props.pdb.resolution ? this.props.pdb.resolution + " Å" : "")}</div>
+                    </div>
                 </div>
-                <div className="row-2">
-                    <div className="title">{ this.props.pdb.title }</div>
+
+                <div className="zincsites">
+                    <ZincSites sites={this.props.pdb.zincsites.edges} />
                 </div>
-                <div className="row-3">
-                    <div className="classification">{ this.props.pdb.classification }</div>
-                    <div className="technique">{ this.props.pdb.technique }</div>
-                    <div className="resolution">{ (this.props.pdb.resolution ? this.props.pdb.resolution + " Å" : "")}</div>
-                </div>
-                <ZincSites sites={this.props.pdb.zincsites.edges} />
+                
+                
                
             </Box></div>
         );
