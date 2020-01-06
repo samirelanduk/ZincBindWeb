8f32ae8d0bc7f87bd5387f29603c95123fdd16a2
Sam Ireland <sam.ireland.uk@gmail.com>
2020-01-06 20:18:21 +0000

    PDB info table

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 
 class Box extends Component {
     render() { 
+        console.log(this.props)
+        let className = "box";
+        if (this.props.className) {
+            className += " " + this.props.className;
+        }
         return (
-            <div className="box">
+            <div className={className}>
                 {this.props.children}
             </div>
         );
--- src/components/Pdb.js
+++ src/components/Pdb.js
@@ -8,7 +8,8 @@ class Pdb extends Component {
     render() {
         const code = this.props.match.params.code;
         const query_string = `{ pdb(id: "${code}") {
-            title
+            title classification keywords depositionDate technique organism
+            expressionSystem assembly resolution rvalue
         }}`
         const QUERY = gql(query_string);
 
@@ -22,7 +23,23 @@ class Pdb extends Component {
                         }
                         return (
                             <Fragment>
-                                <Box><h1>{ data.pdb.title }</h1></Box>
+                                <Box className="heading"><h1>{ data.pdb.title }</h1></Box>
+                                <Box className="properties">
+                                    <div className="pdb-row"><span className="property">Code: </span> <a href={"https://www.rcsb.org/structure/" + code} target="_blank" rel="noopener">{code}</a></div>
+                                    <div className="pdb-row"><span className="property">Deposited: </span> {data.pdb.depositionDate}</div>
+
+                                    <div className="pdb-row"><span className="property">Resolution: </span> {data.pdb.resolution}</div>
+                                    <div className="pdb-row"><span className="property">R-value: </span> {data.pdb.rvalue}</div> 
+
+                                    <div className="pdb-row"><span className="property">Classification: </span> {data.pdb.classification}</div>
+                                    <div className="pdb-row"><span className="property">Biological Assembly: </span> {data.pdb.assembly}</div>
+
+                                    <div className="pdb-row full"><span className="property">Source Organism: </span> <span className="species">{data.pdb.organism}</span></div>
+                                    <div className="pdb-row full"><span className="property">Expression Organism: </span> <span className="species">{data.pdb.expressionSystem}</span></div>
+                                    <div className="pdb-row full"><span className="property">Experimental Technique: </span> {data.pdb.technique}</div>
+                                    <div className="pdb-row full"><span className="property">Keywords: </span> {data.pdb.keywords}</div>
+                                    
+                                </Box>
                             </Fragment>
                         )
                     }
