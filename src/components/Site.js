f6c1cc44b6e461b4056cf2a26b1c9bf9d346bf90
Sam Ireland <sam.ireland.uk@gmail.com>
2020-01-06 23:35:00 +0000

    Basic site info

--- src/components/Site.js
+++ src/components/Site.js
@@ -1,4 +1,5 @@
 import React, { Fragment, Component } from "react";
+import { Link } from "react-router-dom";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Box from "./Box";
@@ -8,7 +9,13 @@ class Site extends Component {
     render() {
         const id = this.props.match.params.id;
         const query_string = `{ zincsite(id: "${id}") {
-            id family
+            id family pdb { id title }
+            metals { count edges { node {
+                id chainId residueNumber insertionCode element coordinateBonds { count }
+            } } }
+            residues(primary: true) { count edges { node {
+                id chainIdentifier residueNumber insertionCode name
+            } } }
         }}`
         const QUERY = gql(query_string);
 
@@ -23,6 +30,36 @@ class Site extends Component {
                         return (
                             <Fragment>
                                 <Box className="heading"><h1>{ data.zincsite.id }</h1></Box>
+
+                                <div className="two-box">
+                                    <Box>
+                                        <h3>PDB</h3>
+                                        <div className="site-pdb">
+                                            <Link to={ "/pdbs/" + data.zincsite.pdb.id }>{ data.zincsite.pdb.id }</Link>: { data.zincsite.pdb.title }
+                                        </div>
+
+                                        <h3>Metals: { data.zincsite.metals.count }</h3>
+                                        <div className="metals">
+                                            { data.zincsite.metals.edges.map(edge => {
+                                                return <div className="metal" key={edge.node.id}>
+                                                    {edge.node.chainId}:{edge.node.residueNumber}{edge.node.insertionCode} ({edge.node.element}, {edge.node.coordinateBonds.count}-coordination)
+                                                </div>
+                                            }) }
+                                        </div>
+
+                                        <h3>Liganding Residues: { data.zincsite.residues.count }</h3>
+                                        <div className="residues">
+                                            { data.zincsite.residues.edges.map(edge => {
+                                                return <div className="residue" key={edge.node.id}>
+                                                    {edge.node.chainIdentifier}:{edge.node.residueNumber}{edge.node.insertionCode} ({edge.node.name})
+                                                </div>
+                                            }) }
+                                        </div>
+                                    </Box>
+                                    <Box>
+                                        
+                                    </Box>
+                                </div>
                             </Fragment>
                         )
                     }
