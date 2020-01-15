b1f80931bea5e2e502bb3f6ed87fe4f31c73efb2
Sam Ireland <sam.ireland.uk@gmail.com>
2020-01-15 12:49:47 +0000

    Missing values in PDB info

--- src/components/PdbInfo.js
+++ src/components/PdbInfo.js
@@ -11,14 +11,14 @@ class PdbInfo extends React.Component {
                 <div className="pdb-row"><span className="property">Code: </span> <a href={"https://www.rcsb.org/structure/" + this.props.code} target="_blank" rel="noopener noreferrer">{this.props.code} <img src={externalLink} alt="external" /></a></div>
                 <div className="pdb-row"><span className="property">Deposited: </span> {this.props.pdb.depositionDate}</div>
 
-                <div className="pdb-row"><span className="property">Resolution: </span> {this.props.pdb.resolution}</div>
-                <div className="pdb-row"><span className="property">R-value: </span> {this.props.pdb.rvalue}</div> 
+                <div className="pdb-row"><span className="property">Resolution: </span> {this.props.pdb.resolution || "N/A"}</div>
+                <div className="pdb-row"><span className="property">R-value: </span> {this.props.pdb.rvalue || "N/A"}</div> 
 
                 <div className="pdb-row"><span className="property">Classification: </span> {this.props.pdb.classification}</div>
-                <div className="pdb-row"><span className="property">Biological Assembly: </span> {this.props.pdb.assembly}</div>
+                <div className="pdb-row"><span className="property">Biological Assembly: </span> {this.props.pdb.assembly || "AU"}</div>
 
-                <div className="pdb-row full"><span className="property">Source Organism: </span> <span className="species">{this.props.pdb.organism}</span></div>
-                <div className="pdb-row full"><span className="property">Expression Organism: </span> <span className="species">{this.props.pdb.expressionSystem}</span></div>
+                <div className="pdb-row full"><span className="property">Source Organism: </span> {this.props.pdb.organism ? <span className="species">{this.props.pdb.organism}</span> : <span>Unknown</span>}</div>
+                <div className="pdb-row full"><span className="property">Expression Organism: </span> {this.props.pdb.expressionSystem ? <span className="species">{this.props.pdb.expressionSystem}</span> : <span>Unknown</span>}</div>
                 <div className="pdb-row full"><span className="property">Experimental Technique: </span> {this.props.pdb.technique}</div>
                 <div className="pdb-row full keywords"><span className="property">Keywords: </span> {this.props.pdb.keywords.split(", ").map((keyword, i) => {
                     return <a key={i} href={`/search?keywords=${keyword}`}>{ keyword }</a>
