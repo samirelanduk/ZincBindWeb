c3a793dab792c5013ba9b87e1e5f687a092c8ded
Sam Ireland <sam.ireland.uk@gmail.com>
2020-01-21 09:16:43 -0800

    Basic predict boxes

--- src/components/Predict.js
+++ src/components/Predict.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactGA from "react-ga";
+import Box from "./Box";
 
 class Predict extends Component {
     constructor(props) {
@@ -11,7 +12,10 @@ class Predict extends Component {
         ReactGA.initialize("UA-51790964-20");
         ReactGA.pageview(window.location.pathname + window.location.search);
 
-        return ( <main>Predict</main> );
+        return ( <main>
+            <Box><h1>Predict Zinc Binding</h1></Box>
+            <Box><p>Coming soon</p></Box>
+        </main> );
     }
 }
  
