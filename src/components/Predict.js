02465ad63bff407bd8bafcdd5972f402650f3ee3
Sam Ireland <sam.ireland.uk@gmail.com>
2020-05-29 00:07:51 +0100

    Sequence prediction interface

--- src/components/Predict.js
+++ src/components/Predict.js
@@ -1,20 +1,50 @@
 import React, { Component } from "react";
 import ReactGA from "react-ga";
+import Select from "react-select";
 import Box from "./Box";
 
 class Predict extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            sequence: ""
+        }
     }
     render() {
         document.title = "Predict - ZincBind";
         ReactGA.initialize("UA-51790964-20");
         ReactGA.pageview(window.location.pathname + window.location.search);
 
-        return ( <main>
+        const families = [
+            {value: "H3", label: "H3"},
+            {value: "C4", label: "C4"},
+            {value: "H2C2", label: "H2C2"},
+        ]
+
+        return ( <main className="predict-page">
             <Box><h1>Predict Zinc Binding</h1></Box>
-            <Box><p>Coming soon</p></Box>
+            <Box>
+                <h2>Sequence Prediction</h2>
+                <form>
+                    <textarea
+                        placeholder="Sequence"
+                        value={this.state.sequence}
+                        onChange={e => this.setState({sequence: e.target.value})}
+                    />
+                    <div className="options">
+                        <Select
+                            isMulti
+                            name="colors"
+                            options={families}
+                            placeholder="Limit families..."
+                            className="families-select"
+                            classNamePrefix="select"
+                        />
+                        <input type="submit" value="Predict" />
+                    </div>
+                </form>
+
+            </Box>
         </main> );
     }
 }
