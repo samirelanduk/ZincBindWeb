147bb4c7744ec92d3cc6e4adede625a59de172d2
Sam Ireland <sam.ireland.uk@gmail.com>
2020-06-04 15:21:22 +0100

    Structure predict interface

--- src/components/Predict.js
+++ src/components/Predict.js
@@ -5,9 +5,12 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from "apollo-link-http";
+import "react-toggle/style.css"
 import ReactGA from "react-ga";
 import Select from "react-select";
+import Toggle from "react-toggle";
 import Box from "./Box";
+import fileUpload from "../images/file-upload.svg"
 
 const SEARCH_SEQUENCE = gql`mutation($sequence: String!, $families: [String]) {
     searchSequence(sequence: $sequence, families: $families) {
@@ -60,31 +63,84 @@ const Predict = props => {
 
     return ( <main className="predict-page">
         <Box><h1>Predict Zinc Binding</h1></Box>
-        <Box>
-            <h2>Sequence Prediction</h2>
-            <form onSubmit={sequenceSubmit}>
-                {error && <div className="error">{ error }</div>}
-                <textarea
-                    className={error ? "errored" : ""}
-                    placeholder="Sequence"
-                    value={sequence}
-                    onChange={e => {setSequence(e.target.value); setError("")}}
-                />
-                <div className="options">
-                    <Select
-                        isMulti
-                        name="colors"
-                        options={families}
-                        placeholder="Limit families..."
-                        onChange={familiesUpdated}
-                        className="families-select"
-                        classNamePrefix="select"
-                    />
-                    <input type="submit" value="Predict" />
-                </div>
-            </form>
 
-        </Box>
+        <div className="predict-grid">
+            <Box>
+                <h2>Sequence Prediction</h2>
+                <form onSubmit={sequenceSubmit}>
+                    {error && <div className="error">{ error }</div>}
+                    <textarea
+                        className={error ? "errored" : ""}
+                        placeholder="Sequence"
+                        value={sequence}
+                        onChange={e => {setSequence(e.target.value); setError("")}}
+                    />
+                    <div className="options">
+                        <Select
+                            isMulti
+                            name="colors"
+                            options={families}
+                            placeholder="Limit families..."
+                            onChange={familiesUpdated}
+                            className="families-select"
+                            classNamePrefix="select"
+                        />
+                        <input type="submit" value="Predict" />
+                    </div>
+                </form>
+            </Box>
+
+            <Box>
+                <h2>Structure Prediction</h2>
+                <form onSubmit={sequenceSubmit}>
+                    {error && <div className="error">{ error }</div>}
+                    
+                    <div className="file-upload">
+                        <img src={fileUpload} />
+                        <label>Upload Structure File (.pdb, .cif or .mmtf)</label>
+                    </div>
+                    <div className="toggles">
+                        <div className="toggle">
+                            <Toggle
+                                id="location"
+                                defaultChecked={true}
+                                
+                            />
+                            <label htmlFor="location">Search by location</label>
+                        </div>
+                        <div className="toggle">
+                            <Toggle
+                                id="family"
+                                defaultChecked={true}
+                                
+                            />
+                            <label htmlFor="location">Search by family</label>
+                        </div>
+                        <div className="toggle">
+                            <Toggle
+                                id="half"
+                                defaultChecked={false}
+                                
+                            />
+                            <label htmlFor="location">Search for half-sites</label>
+                        </div>
+
+                    </div>
+                    <div className="options">
+                        <Select
+                            isMulti
+                            name="colors"
+                            options={families}
+                            placeholder="Limit families..."
+                            onChange={familiesUpdated}
+                            className="families-select"
+                            classNamePrefix="select"
+                        />
+                        <input type="submit" value="Predict" />
+                    </div>
+                </form>
+            </Box>
+        </div>
     </main> );
 }
  
