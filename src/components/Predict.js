fc61416254d3c43e48d32b917d682d0eb4b5341d
Sam Ireland <sam.ireland.uk@gmail.com>
2020-06-05 19:38:05 +0100

    Structure upload mutation

--- src/components/Predict.js
+++ src/components/Predict.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { createUploadLink } from "apollo-upload-client";
 import { ApolloLink } from 'apollo-link';
 import { createHttpLink } from "apollo-link-http";
 import "react-toggle/style.css"
@@ -18,44 +19,103 @@ const SEARCH_SEQUENCE = gql`mutation($sequence: String!, $families: [String]) {
     }
 }`
 
+const SEARCH_STRUCTURE = gql`mutation(
+    $structure: Upload!, $families: [String], $findHalf: Boolean,
+    $useFamiliesModels: Boolean, $useLocationModels: Boolean
+) {
+    searchStructure(
+        structure: $structure, families: $families, findHalf: $findHalf,
+        useFamiliesModels: $useFamiliesModels, useLocationModels: $useLocationModels
+    ) { jobId }
+}`
+
+const uploadLink = createUploadLink({
+    uri: "http://localhost:7001", headers: {"keep-alive": "true"}
+})
 const httpLink = createHttpLink({ uri: "http://localhost:7001/" });
-const link = ApolloLink.from([httpLink]);
+const link = ApolloLink.from([uploadLink, httpLink]);
 const predictClient = new ApolloClient({cache: new InMemoryCache(), link: link});
 
 const Predict = props => {
-    const [sequence, setSequence] = useState("");
-    const [selectedFamilies, setSelectedFamilies] = useState([]);
-    const [error, setError] = useState("");
-
-
     document.title = "Predict - ZincBind";
     ReactGA.initialize("UA-51790964-20");
     ReactGA.pageview(window.location.pathname + window.location.search);
 
-    const families = [
+    const [sequence, setSequence] = useState("");
+    const [selectedSequenceFamilies, setSelectedSequenceFamilies] = useState([]);
+    const [sequenceError, setSequenceError] = useState("");
+
+    const fileRef = useRef(null);
+    const [fileName, setFileName] = useState("");
+    const [selectedStructureFamilies, setSelectedStructureFamilies] = useState([]);
+    const [searchByLocation, setSearchByLocation] = useState(true);
+    const [searchByFamily, setSearchByFamily] = useState(true);
+    const [searchHalfSites, setSearchHalfSites] = useState(false);
+    const [structureError, setStructureError] = useState("");
+
+    const sequenceFamilies = [
         {value: "H3", label: "H3"},
         {value: "C4", label: "C4"},
         {value: "H2C2", label: "H2C2"},
     ]
 
+    const structureFamilies = [
+        {value: "H3", label: "H3"},
+        {value: "C4", label: "C4"},
+        {value: "H2C2", label: "H2C2"},
+    ]
+
+    const sequenceFamiliesUpdated = selection => {
+        setSelectedSequenceFamilies(selection.map(s => s.value))
+    }
+
+    const structureFileAdded = e => {
+        if (e.target.files.length) {
+            setFileName(e.target.files[0].name);
+        }
+    }
+
+    const structureFamiliesUpdated = selection => {
+        setSelectedStructureFamilies(selection.map(s => s.value))
+    }
+
     const [searchSequence, searchSequenceMutation] = useMutation(SEARCH_SEQUENCE, {
         client: predictClient,
-        onError: () => setError("Sorry, an error occured."),
+        onError: () => setSequenceError("Sorry, an error occured."),
         onCompleted: response => {
             props.history.push(`/sequence-jobs/${response.searchSequence.jobId}/`);
         }
     });
 
-    const familiesUpdated = selection => {
-        setSelectedFamilies(selection.map(s => s.value))
-    }
-
+    const [searchStructure, searchStructureMutation] = useMutation(SEARCH_STRUCTURE, {
+        client: predictClient,
+        onError: () => setSequenceError("Sorry, an error occured."),
+        onCompleted: response => {
+            //props.history.push(`/structure-jobs/${response.searchStructure.jobId}/`);
+        }
+    });
+    
     const sequenceSubmit = e => {
         e.preventDefault();
         if (sequence.length) {
             searchSequence({
                 variables: {
-                    sequence: sequence, families: selectedFamilies
+                    sequence: sequence, families: selectedSequenceFamilies
+                },
+            })
+        }
+    }
+
+    const structureSubmit = e => {
+        e.preventDefault();
+        if (fileRef.current.files.length) {
+            searchStructure({
+                variables: {
+                    structure: fileRef.current.files[0],
+                    families: selectedStructureFamilies,
+                    findHalf: searchHalfSites,
+                    useFamiliesModels: searchByFamily,
+                    useLocationModels: searchByLocation
                 },
             })
         }
@@ -68,20 +128,20 @@ const Predict = props => {
             <Box>
                 <h2>Sequence Prediction</h2>
                 <form onSubmit={sequenceSubmit}>
-                    {error && <div className="error">{ error }</div>}
+                    {sequenceError && <div className="error">{ sequenceError }</div>}
                     <textarea
-                        className={error ? "errored" : ""}
+                        className={sequenceError ? "errored" : ""}
                         placeholder="Sequence"
                         value={sequence}
-                        onChange={e => {setSequence(e.target.value); setError("")}}
+                        onChange={e => {setSequence(e.target.value); setSequenceError("")}}
                     />
                     <div className="options">
                         <Select
                             isMulti
                             name="colors"
-                            options={families}
+                            options={sequenceFamilies}
                             placeholder="Limit families..."
-                            onChange={familiesUpdated}
+                            onChange={sequenceFamiliesUpdated}
                             className="families-select"
                             classNamePrefix="select"
                         />
@@ -92,35 +152,35 @@ const Predict = props => {
 
             <Box>
                 <h2>Structure Prediction</h2>
-                <form onSubmit={sequenceSubmit}>
-                    {error && <div className="error">{ error }</div>}
-                    
+                <form onSubmit={structureSubmit}>
+                    {structureError && <div className="error">{ structureError }</div>}
                     <div className="file-upload">
-                        <img src={fileUpload} />
-                        <label>Upload Structure File (.pdb, .cif or .mmtf)</label>
+                        <label for="file"><img src={fileUpload} /></label>
+                        <input type="file" id="file" ref={fileRef} onChange={structureFileAdded} />
+                        <label>{fileName || "Upload Structure File (.pdb, .cif or .mmtf)"}</label>
                     </div>
                     <div className="toggles">
                         <div className="toggle">
                             <Toggle
                                 id="location"
-                                defaultChecked={true}
-                                
+                                checked={searchByLocation}
+                                onChange={() => setSearchByLocation(!searchByLocation)}
                             />
                             <label htmlFor="location">Search by location</label>
                         </div>
                         <div className="toggle">
                             <Toggle
                                 id="family"
-                                defaultChecked={true}
-                                
+                                checked={searchByFamily} 
+                                onChange={() => setSearchByFamily(!searchByFamily)}
                             />
                             <label htmlFor="location">Search by family</label>
                         </div>
                         <div className="toggle">
                             <Toggle
                                 id="half"
-                                defaultChecked={false}
-                                
+                                checked={searchHalfSites}
+                                onChange={() => setSearchHalfSites(!searchHalfSites)}
                             />
                             <label htmlFor="location">Search for half-sites</label>
                         </div>
@@ -130,9 +190,9 @@ const Predict = props => {
                         <Select
                             isMulti
                             name="colors"
-                            options={families}
+                            options={structureFamilies}
                             placeholder="Limit families..."
-                            onChange={familiesUpdated}
+                            onChange={structureFamiliesUpdated}
                             className="families-select"
                             classNamePrefix="select"
                         />
